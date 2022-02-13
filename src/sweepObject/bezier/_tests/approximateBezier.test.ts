b2d43e1343697e8fe26c595aa189736fa3de792f
TarVK <tarvankrieken@gmail.com>
2022-02-13 19:21:56 +0100

    Wrote a simple test

---
+++ src/sweepObject/bezier/_tests/approximateBezier.test.ts
@@ -0,0 +1,31 @@
+import {Vec2} from "../../../util/Vec2";
+import {approximateBezier} from "../approximateBezier";
+
+describe("approximateBezier", () => {
+    it("evenly spaces points", () => {
+        const targetPrecision = 0.01; // 1% off or less
+        const targetSpacing = 0.1;
+        const nodes = approximateBezier(
+            {
+                start: new Vec2(0, 0),
+                startControl: new Vec2(0.5, 0.5),
+                endControl: new Vec2(0.5, 0),
+                end: new Vec2(1, 0),
+            },
+            {
+                spacing: targetSpacing,
+            }
+        );
+
+        const distances = nodes
+            .slice(0, -2)
+            .map(({point}, i) => nodes[i + 1].point.sub(point).length());
+        const outOfRangeDistances = distances.filter(
+            distance =>
+                Math.abs((distance - targetSpacing) / targetSpacing) > targetPrecision
+        );
+
+        expect(nodes.length).toBeGreaterThan(5);
+        expect(outOfRangeDistances.length).toBe(0);
+    });
+});
