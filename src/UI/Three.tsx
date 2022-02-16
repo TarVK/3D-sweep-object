8584325cfae77b92a31f3ed273c589ce9f6346b6
georgi kostov <georgi.kostov@cytosmart.com>
2022-02-16 19:40:15 +0100

    Adding line and skybox drawings

--- src/UI/Three.tsx
+++ src/UI/Three.tsx
@@ -2,7 +2,12 @@ import React, {Component} from "react";
 import * as THREE from "three";
 //@ts-ignore
 import OrbitControls from "three-orbitcontrols";
-
+import flameBK from "../../public/flame/flame_bk.jpg";
+import flameDN from "../../public/flame/flame_dn.jpg";
+import flameFT from "../../public/flame/flame_ft.jpg";
+import flameLF from "../../public/flame/flame_lf.jpg";
+import flameRT from "../../public/flame/flame_rt.jpg";
+import flameUP from "../../public/flame/flame_up.jpg";
 
 const polygonToPositionCoordinates = (polygon:[number, number][], posZ = 1) => {
     const vecArr = polygon.map(point => new THREE.Vector2(point[0], point[1]), []);
@@ -46,6 +51,30 @@ class ThreeScene extends Component{
         this.scene.add(mesh);
     }
 
+    addLineToScene = (points:[number, number, number][], color = 0xff0000) => {
+        const vecPoints = points.map(p => new THREE.Vector3(p[0], p[1], p[2]));
+        const geometry = new THREE.BufferGeometry().setFromPoints(vecPoints);
+        const material = new THREE.MeshBasicMaterial( { 
+            color,
+            // wireframe: true,
+            side: THREE.DoubleSide
+        });
+        const line = new THREE.Line(geometry, material);
+        // vertices stored here
+        console.log(line.geometry.attributes.position.array);
+        // order of vertices stored here (if null, order is as seen in vertices)
+        console.log(line.geometry.index);
+        this.scene.add(line);
+    }
+
+    addSkybox = () => {
+        const imgs = [flameFT, flameBK, flameUP, flameDN, flameRT, flameLF];
+        const textures = imgs.map(i => new THREE.TextureLoader().load(i));
+        const materials = textures.map(t => new THREE.MeshBasicMaterial({ map: t, side: THREE.BackSide }));
+        const skyboxGeo = new THREE.BoxGeometry(1000, 1000, 1000);
+        const skybox = new THREE.Mesh(skyboxGeo, materials);
+        this.scene.add(skybox);
+    }
     initThreeJS = () => {
         const width = window.innerWidth - 200;
         const height = window.innerHeight - 200;
@@ -59,11 +88,14 @@ class ThreeScene extends Component{
         this.camera = new THREE.PerspectiveCamera(90, width / height, 0.1, 1000);
         this.camera.position.z = 5;
         
+        
+        this.addSkybox();
         this.addGridToScene();
         this.addCubeToScene();
         this.addPolygonToScene([[0,0], [1,1], [0,1]], 1);
         this.addPolygonToScene([[0,0], [1,0], [1,1], [0,1]], 2, 0xdd22aa);
         this.addPolygonToScene([[0,0], [0,1], [0.5, 0.5], [1,1], [1,0]], 3, 0xbbaa44);
+        this.addLineToScene([[1,1,4], [1,2,4], [1.5, 1.5,4], [2,2,4], [2,1,4]]);
         
         this.animate();
         new OrbitControls(this.camera, this.renderer.domElement);
