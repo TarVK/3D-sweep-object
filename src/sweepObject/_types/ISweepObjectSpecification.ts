e666edbc4fcf2f07046814abfeb30b302df9c8c0
georgi kostov <georgi.kostov@cytosmart.com>
2022-02-18 15:36:38 +0100

    Adding sweep line algorithm and light source. All is very buggy and case insensitive tho

--- src/sweepObject/_types/ISweepObjectSpecification.ts
+++ src/sweepObject/_types/ISweepObjectSpecification.ts
@@ -1,6 +1,11 @@
-import {ISweepLine} from "./ISweepLine";
+import { Vec3 } from "../../util/Vec3";
+// import { ICrossSection } from "./ICrossSection";
+// import {ISweepLine} from "./ISweepLine";
 
 export type ISweepObjectSpecification = {
     /** The sweep line to be used for the object */
-    sweepLine: ISweepLine;
+    // sweepLine: ISweepLine;
+    // crossSection: ICrossSection;
+    sweepLine: Vec3[];
+    crossSection: Vec3[];
 };
