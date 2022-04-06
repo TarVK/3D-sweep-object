c26d8e90e757a8711769b9a2a620832a1da6871f
TarVK <tarvankrieken@gmail.com>
2022-04-06 18:35:27 +0200

    Added low-poly shading style

--- src/UI/editors/3D/SweepObject.ts
+++ src/UI/editors/3D/SweepObject.ts
@@ -12,20 +12,47 @@ export class SweepObject extends THREE.Object3D implements IMateriable {
      * @param newMesh The new mesh to be set
      */
     public updateMesh(newMesh: IMesh, castShadow = true) {
+        // TODO: turn into a proper variable with controls
+        const smoothLighting = false;
         const geometry = new THREE.BufferGeometry();
-        geometry.setAttribute(
-            "position",
-            new THREE.Float32BufferAttribute(
-                newMesh.points.flatMap(v => [v.x, v.y, v.z]), // Note we negate z because positive z comes towards the camera instead of away from it, which is counter intuitive
-                3
-            )
-        );
-        geometry.setIndex(newMesh.faces.flat());
+
+        if (smoothLighting) {
+            geometry.setAttribute(
+                "position",
+                new THREE.Float32BufferAttribute(
+                    newMesh.points.flatMap(v => [v.x, v.y, v.z]),
+                    3
+                )
+            );
+            geometry.setIndex(newMesh.faces.flat());
+        } else {
+            // Duplicate vertices for each face, ensuring normals are computed for each face separately
+            const meshVertices = newMesh.points.map(v => [v.x, v.y, v.z]);
+            let count = 0;
+            const vertices: number[] = [];
+            const faces: number[] = [];
+            for (let [i1, i2, i3] of newMesh.faces) {
+                const p1 = meshVertices[i1];
+                const p2 = meshVertices[i2];
+                const p3 = meshVertices[i3];
+                vertices.push(...p1, ...p2, ...p3);
+                faces.push(count++, count++, count++);
+            }
+
+            geometry.setAttribute(
+                "position",
+                new THREE.Float32BufferAttribute(vertices, 3)
+            );
+            geometry.setIndex(faces);
+            console.log(vertices, faces);
+        }
+
         geometry.computeVertexNormals();
         const material = new THREE.MeshStandardMaterial({
             color: colors.SWEEP_OBJECT,
-            roughness: 0.2,
+            roughness: 0.5,
             metalness: 0.8,
+            flatShading: true,
             wireframe: this.wireframe,
             // side: THREE.DoubleSide,
         });
