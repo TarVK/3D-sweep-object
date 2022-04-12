3cbc59691a6693ca2da7314c59b75146c79784fd
TarVK <tarvankrieken@gmail.com>
2022-04-12 22:25:42 +0200

    Fixed arc drawing

--- src/UI/editors/3D/CrossSection.ts
+++ src/UI/editors/3D/CrossSection.ts
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import {BezierSegmentState} from "../../../state/segments/BezierSegmentState";
-import { StraightSegmentState } from "../../../state/segments/StraightSegmentState";
-import { ArcSegmentState } from "../../../state/segments/ArcSegmentState";
+import {StraightSegmentState} from "../../../state/segments/StraightSegmentState";
+import {ArcSegmentState} from "../../../state/segments/ArcSegmentState";
 import {ISegment} from "../../../state/_types/ISegment";
 import {Vec2} from "../../../util/linearAlgebra/Vec2";
 import {colors} from "./ColorSchema";
@@ -11,12 +11,27 @@ export class CrossSection extends THREE.Object3D implements IMateriable {
     public updateMaterial(material: THREE.Material): void {}
 
     public updateCrossSection(segments: ISegment<Vec2>[]) {
-        const material = new THREE.LineBasicMaterial( { color: colors.SWEEP_LINE } );
+        const material = new THREE.LineBasicMaterial({color: colors.SWEEP_LINE});
 
+        this.remove(...this.children);
         // const crossSectionShape = new THREE.Shape();
         // const material = new THREE.LineBasicMaterial({color: colors.SWEEP_LINE});
 
         segments.forEach(segment => {
+            const addStraightLine = () => {
+                const start = segment.getStart();
+                const end = segment.getEnd();
+
+                const points = [
+                    new THREE.Vector3(start.x, start.y, 0),
+                    new THREE.Vector3(end.x, end.y, 0),
+                ];
+                const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+                const straightObject = new THREE.Line(geometry, material);
+                this.add(straightObject);
+            };
+
             if (segment instanceof BezierSegmentState) {
                 const start = segment.getStart();
                 const end = segment.getEnd();
@@ -24,55 +39,47 @@ export class CrossSection extends THREE.Object3D implements IMateriable {
                 const endControl = segment.getEndControl();
 
                 const curve = new THREE.CubicBezierCurve3(
-                    new THREE.Vector3( start.x, start.y, 2 ),
-                    new THREE.Vector3( startControl.x, startControl.y, 2 ),
-                    new THREE.Vector3( endControl.x, endControl.y, 2 ),
-                    new THREE.Vector3( end.x, end.y, 2 )
+                    new THREE.Vector3(start.x, start.y, 0),
+                    new THREE.Vector3(startControl.x, startControl.y, 0),
+                    new THREE.Vector3(endControl.x, endControl.y, 0),
+                    new THREE.Vector3(end.x, end.y, 0)
                 );
 
+                const points = curve.getPoints(50);
+                const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
-                const points = curve.getPoints( 50 );
-                const geometry = new THREE.BufferGeometry().setFromPoints( points );
-                                
                 // Create the final object to add to the scene
-                const curveObject = new THREE.Line( geometry, material );
+                const curveObject = new THREE.Line(geometry, material);
                 this.add(curveObject);
-            }
+            } else if (segment instanceof StraightSegmentState) {
+                addStraightLine();
+            } else if (segment instanceof ArcSegmentState) {
+                const spec = segment.getSpec();
 
-            else if (segment instanceof StraightSegmentState) {
-        
-                const start = segment.getStart();
-                const end = segment.getEnd();
+                if (!spec.origin) {
+                    addStraightLine();
+                    return;
+                }
 
-                const points = [
-                    new THREE.Vector3(start.x, start.y, 2),
-                    new THREE.Vector3(end.x, end.y, 2)
-                ]
-                const geometry = new THREE.BufferGeometry().setFromPoints( points );                
-                
-                const straightObject = new THREE.Line( geometry, material );
-                this.add(straightObject);
-            }
+                const {delta, origin, start, radius} = spec;
 
-            else if (segment instanceof ArcSegmentState) {
-                console.log("tuka sme")
-                const start = segment.getStart();
-                const end = segment.getEnd();
-                const control = segment.getControl();
-
-                var curve = new THREE.EllipseCurve(
-                    start.x, start.y,
-                    control.getAngle(), control.getAngle(),
-                    start.getAngle(), end.getAngle(),
-                    false, 0
+                const curve = new THREE.EllipseCurve(
+                    origin.x,
+                    origin.y,
+                    radius,
+                    radius,
+                    start,
+                    start + delta,
+                    delta < 0,
+                    0
                 );
-                
-                const points = curve.getPoints( 50 );
-                const geometry = new THREE.BufferGeometry().setFromPoints( points );
-                                
+
+                const points = curve.getPoints(50);
+                const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
                 // Create the final object to add to the scene
-                const ellipse = new THREE.Line( geometry, material );
-                this.add(ellipse)
+                const ellipse = new THREE.Line(geometry, material);
+                this.add(ellipse);
             }
         });
     }
